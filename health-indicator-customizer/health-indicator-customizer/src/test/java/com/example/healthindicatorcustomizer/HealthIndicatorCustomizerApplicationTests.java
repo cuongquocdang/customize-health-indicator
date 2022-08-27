9f1605091232b1b8f3c5a3a63e4c906eabf352ad
Cuong Quoc Dang <cuongdang.developer@gmail.com>
2022-08-27 10:09:02 +0700

    chore: init project

---
+++ health-indicator-customizer/health-indicator-customizer/src/test/java/com/example/healthindicatorcustomizer/HealthIndicatorCustomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthindicatorcustomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthIndicatorCustomizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
